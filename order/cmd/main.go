cedc8de938bb2f05385a3850b9fd1380e6b2b0db
Bruce Neco <bruce.neco@gmail.com>
2024-04-17 22:02:51 -0300

    feat(order): add modules to fx and start grpc server

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"github.com/bruceneco/go-ms-grpc/order/config"
+	"github.com/bruceneco/go-ms-grpc/order/internal/adapters"
+	"github.com/bruceneco/go-ms-grpc/order/internal/adapters/grpc"
+	"github.com/bruceneco/go-ms-grpc/order/internal/application/core"
 	"go.uber.org/fx"
 )
 
 func main() {
-	app := fx.New(config.Module)
+	app := fx.New(
+		config.Module,
+		adapters.Module,
+		core.Module,
+		fx.Invoke(func(server *grpc.Adapter) {
+			server.Run()
+		}),
+	)
 
 	app.Run()
 }
