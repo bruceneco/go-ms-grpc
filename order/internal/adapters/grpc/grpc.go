b0476f3ff290eb14de6080a6da9f0f0fecc60f93
Bruce Neco <bruce.neco@gmail.com>
2024-04-17 22:02:09 -0300

    refactor(order): change grpc adapter to fit on fx

--- order/internal/adapters/grpc/grpc.go
+++ order/internal/adapters/grpc/grpc.go
@@ -3,24 +3,33 @@ package grpc
 import (
 	"context"
 	"fmt"
+	"github.com/bruceneco/go-ms-grpc/order/config"
 	"github.com/bruceneco/go-ms-grpc/order/internal/application/core/domain"
 	"github.com/bruceneco/go-ms-grpc/order/internal/ports"
 	ordergrpc "github.com/bruceneco/go-ms-grpc/proto/go/order"
+	"go.uber.org/fx"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
-	"os"
+	"strconv"
 )
 
 type Adapter struct {
 	api  ports.APIPort
 	port uint16
+	cfg  *config.Config
 	ordergrpc.UnimplementedOrderServer
 }
 
-func NewAdapter(api ports.APIPort, port uint16) *Adapter {
-	return &Adapter{api: api, port: port}
+func NewAdapter(api ports.APIPort, cfg *config.Config, lc fx.Lifecycle) *Adapter {
+	port, err := strconv.Atoi(cfg.GetString(config.EnvAppPort))
+	if err != nil {
+		panic(fmt.Errorf("port must be an integer: %v\n", err))
+	}
+
+	adapter := &Adapter{api: api, port: uint16(port), cfg: cfg}
+	return adapter
 }
 
 func (a *Adapter) Run() {
@@ -31,10 +40,10 @@ func (a *Adapter) Run() {
 	}
 	grpcServer := grpc.NewServer()
 	ordergrpc.RegisterOrderServer(grpcServer, a)
-	// TODO: use viper for env loading
-	if os.Getenv("ENV") == "development" {
+	if a.cfg.GetString(config.EnvMode) == "development" {
 		reflection.Register(grpcServer)
 	}
+	log.Printf("starting grpc server on port %v\n", a.port)
 	if err := grpcServer.Serve(listen); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
