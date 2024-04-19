236d490903d57624317185fcd5322e29c34f3139
Bruce Neco <bruce.neco@gmail.com>
2024-04-19 13:08:46 -0300

    feat(order): add payment to adapters module

--- order/internal/adapters/module.go
+++ order/internal/adapters/module.go
@@ -3,10 +3,12 @@ package adapters
 import (
 	"github.com/bruceneco/go-ms-grpc/order/internal/adapters/db"
 	"github.com/bruceneco/go-ms-grpc/order/internal/adapters/grpc"
+	"github.com/bruceneco/go-ms-grpc/order/internal/adapters/payment"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Provide(
 	grpc.NewAdapter,
 	db.NewAdapter,
+	payment.NewAdapter,
 )
