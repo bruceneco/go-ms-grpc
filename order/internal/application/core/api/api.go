dd9780c0fe315088b5ec3f039683284cb8599873
Bruce Neco <bruce.neco@gmail.com>
2024-04-19 13:09:18 -0300

    feat(order): add payment service call to api

--- order/internal/application/core/api/api.go
+++ order/internal/application/core/api/api.go
@@ -6,12 +6,14 @@ import (
 )
 
 type Application struct {
-	db ports.DBPort
+	db      ports.DBPort
+	payment ports.PaymentPort
 }
 
-func NewApplication(db ports.DBPort) ports.APIPort {
+func NewApplication(db ports.DBPort, payment ports.PaymentPort) ports.APIPort {
 	return &Application{
 		db,
+		payment,
 	}
 }
 
@@ -19,6 +21,10 @@ func (a *Application) PlaceOrder(order *domain.Order) (*domain.Order, error) {
 	err := a.db.Save(order)
 	if err != nil {
 		return nil, err
+	}
+	paymentErr := a.payment.Charge(order)
+	if paymentErr != nil {
+		return nil, paymentErr
 	}
 	return order, nil
 }
